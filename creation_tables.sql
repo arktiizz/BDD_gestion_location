c265ce04bf5bd778abe91098a20959c2c7d85dd6
The Lan Nguyen <nguyen322u@etu.univ-lorraine.fr>
2026-02-16 11:35:36 +0100

    création table: données de bases

--- creation_tables.sql
+++ creation_tables.sql
@@ -57,3 +57,71 @@ CREATE TABLE Location (
 
     CONSTRAINT check_DateRetour CHECK (DateRetour > DateDepart)
 );
+
+commit;
+
+INSERT INTO Categories (NumCat, Categorie, PrixKm) VALUES (1, 'Citadine', 0.3);
+INSERT INTO Categories (NumCat, Categorie, PrixKm) VALUES (2, 'Berline', 0.4);
+INSERT INTO Categories (NumCat, Categorie, PrixKm) VALUES (3, 'Monospace', 0.4);
+INSERT INTO Categories (NumCat, Categorie, PrixKm) VALUES (4, 'SUV', 0.4);
+INSERT INTO Categories (NumCat, Categorie, PrixKm) VALUES (5, '3m3', 0.3);
+INSERT INTO Categories (NumCat, Categorie, PrixKm) VALUES (6, '9m3', 0.4);
+INSERT INTO Categories (NumCat, Categorie, PrixKm) VALUES (7, '14m3', 0.45);
+
+INSERT INTO Modeles (Modele, Marque, NumCat) VALUES ('CLIO', 'RENAULT', 1);
+INSERT INTO Modeles (Modele, Marque, NumCat) VALUES ('208', 'PEUGEOT', 1);
+INSERT INTO Modeles (Modele, Marque, NumCat) VALUES ('C3', 'CITROEN', 1);
+INSERT INTO Modeles (Modele, Marque, NumCat) VALUES ('A4', 'AUDI', 2);
+INSERT INTO Modeles (Modele, Marque, NumCat) VALUES ('508', 'PEUGEOT', 2);
+INSERT INTO Modeles (Modele, Marque, NumCat) VALUES ('PICASSO', 'CITROEN', 3);
+INSERT INTO Modeles (Modele, Marque, NumCat) VALUES ('SCENIC', 'RENAULT', 3);
+INSERT INTO Modeles (Modele, Marque, NumCat) VALUES ('5008', 'PEUGEOT', 4);
+INSERT INTO Modeles (Modele, Marque, NumCat) VALUES ('KANGOO', 'RENAULT', 5);
+INSERT INTO Modeles (Modele, Marque, NumCat) VALUES ('TRANSIT', 'FORD', 6);
+INSERT INTO Modeles (Modele, Marque, NumCat) VALUES ('MASTER', 'RENAULT', 7);
+INSERT INTO Modeles (Modele, Marque, NumCat) VALUES ('TIGUAN', 'VW', 4);
+INSERT INTO Modeles (Modele, Marque, NumCat) VALUES ('VITO', 'MERCEDES', 6);
+INSERT INTO Modeles (Modele, Marque, NumCat) VALUES ('DUCATO', 'FIAT', 1); 
+
+INSERT INTO Formules (Formule, NbJours, ForfaitKm) VALUES ('jour', 1, 100);
+INSERT INTO Formules (Formule, NbJours, ForfaitKm) VALUES ('fin-semaine', 2, 200);
+INSERT INTO Formules (Formule, NbJours, ForfaitKm) VALUES ('semaine', 7, 500);
+INSERT INTO Formules (Formule, NbJours, ForfaitKm) VALUES ('mois', 30, 1500);
+
+-- Catégorie 1 (Citadine)
+INSERT INTO Tarifs (NumCat, Formule, Tarif) VALUES (1, 'jour', 39);
+INSERT INTO Tarifs (NumCat, Formule, Tarif) VALUES (1, 'fin-semaine', 69);
+INSERT INTO Tarifs (NumCat, Formule, Tarif) VALUES (1, 'semaine', 199);
+INSERT INTO Tarifs (NumCat, Formule, Tarif) VALUES (1, 'mois', 499);
+-- Catégorie 2 (Berline)
+INSERT INTO Tarifs (NumCat, Formule, Tarif) VALUES (2, 'jour', 59);
+INSERT INTO Tarifs (NumCat, Formule, Tarif) VALUES (2, 'fin-semaine', 99);
+INSERT INTO Tarifs (NumCat, Formule, Tarif) VALUES (2, 'semaine', 299);
+INSERT INTO Tarifs (NumCat, Formule, Tarif) VALUES (2, 'mois', 799);
+-- Catégorie 3 (Monospace)
+INSERT INTO Tarifs (NumCat, Formule, Tarif) VALUES (3, 'jour', 69);
+INSERT INTO Tarifs (NumCat, Formule, Tarif) VALUES (3, 'fin-semaine', 129);
+INSERT INTO Tarifs (NumCat, Formule, Tarif) VALUES (3, 'semaine', 499);
+INSERT INTO Tarifs (NumCat, Formule, Tarif) VALUES (3, 'mois', 1099);
+-- Catégorie 4 (SUV)
+INSERT INTO Tarifs (NumCat, Formule, Tarif) VALUES (4, 'jour', 69);
+INSERT INTO Tarifs (NumCat, Formule, Tarif) VALUES (4, 'fin-semaine', 129);
+INSERT INTO Tarifs (NumCat, Formule, Tarif) VALUES (4, 'semaine', 499);
+INSERT INTO Tarifs (NumCat, Formule, Tarif) VALUES (4, 'mois', 1099);
+-- Catégorie 5 (3m3)
+INSERT INTO Tarifs (NumCat, Formule, Tarif) VALUES (5, 'jour', 39);
+INSERT INTO Tarifs (NumCat, Formule, Tarif) VALUES (5, 'fin-semaine', 79);
+INSERT INTO Tarifs (NumCat, Formule, Tarif) VALUES (5, 'semaine', 199);
+INSERT INTO Tarifs (NumCat, Formule, Tarif) VALUES (5, 'mois', 599);
+-- Catégorie 6 (9m3)
+INSERT INTO Tarifs (NumCat, Formule, Tarif) VALUES (6, 'jour', 49);
+INSERT INTO Tarifs (NumCat, Formule, Tarif) VALUES (6, 'fin-semaine', 99);
+INSERT INTO Tarifs (NumCat, Formule, Tarif) VALUES (6, 'semaine', 259);
+INSERT INTO Tarifs (NumCat, Formule, Tarif) VALUES (6, 'mois', 899);
+-- Catégorie 7 (14m3)
+INSERT INTO Tarifs (NumCat, Formule, Tarif) VALUES (7, 'jour', 79);
+INSERT INTO Tarifs (NumCat, Formule, Tarif) VALUES (7, 'fin-semaine', 159);
+INSERT INTO Tarifs (NumCat, Formule, Tarif) VALUES (7, 'semaine', 359);
+INSERT INTO Tarifs (NumCat, Formule, Tarif) VALUES (7, 'mois', 1199);
+
+commit;
