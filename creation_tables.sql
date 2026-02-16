8d1491ee9971109f5fb67245f8a1fe1d98778597
aurekbs <87376879+aurekbs@users.noreply.github.com>
2026-02-16 10:34:09 +0100

    Create creation_tables.sql
    Création des tables de la BDD en prenant en compte les contraintes d'intégrité

---
+++ creation_tables.sql
@@ -0,0 +1,57 @@
+DROP TABLE Categories CASCADE CONSTRAINTS;
+DROP TABLE Modeles CASCADE CONSTRAINTS;
+DROP TABLE Vehicule CASCADE CONSTRAINTS;
+DROP TABLE VehiculeRetraite CASCADE CONSTRAINTS;
+DROP TABLE Formules CASCADE CONSTRAINTS;
+DROP TABLE Tarifs CASCADE CONSTRAINTS;
+DROP TABLE Location CASCADE CONSTRAINTS;
+
+CREATE TABLE Categories (
+    NumCat NUMBER PRIMARY KEY,
+    Categorie VARCHAR2(100),
+    PrixKm NUMBER NOT NULL
+);
+
+CREATE TABLE Modeles ( 
+    Modele VARCHAR2(100) PRIMARY KEY,
+    Marque VARCHAR2(100) NOT NULL,
+    NumCat NUMBER NOT NULL REFERENCES Categories(NumCat)
+);
+
+CREATE TABLE Vehicule (
+    NumVeh NUMBER PRIMARY KEY,
+    Modele VARCHAR2(100) NOT NULL REFERENCES Modeles(Modele),
+    Km NUMBER NOT NULL,
+    Situation VARCHAR2(100) NOT NULL,
+    NbJoursLoc NUMBER DEFAULT 0 NOT NULL,
+    CAV NUMBER DEFAULT 0 NOT NULL
+);
+
+CREATE TABLE VehiculeRetraite (
+    NumVeh NUMBER PRIMARY KEY REFERENCES Vehicule(NumVeh), 
+    DateRetraite DATE NOT NULL
+);
+
+CREATE TABLE Formules (
+    Formule VARCHAR2(100) PRIMARY KEY,
+    NbJours NUMBER NOT NULL,
+    ForfaitKm NUMBER NOT NULL 
+);
+
+CREATE TABLE Tarifs (
+    NumCat NUMBER REFERENCES Categories(NumCat),
+    Formule VARCHAR2(100) REFERENCES Formules(Formule),
+    Tarif NUMBER NOT NULL
+);
+
+CREATE TABLE Location (
+    NumLoc NUMBER PRIMARY KEY,
+    NumVeh NUMBER REFERENCES Vehicule(NumVeh),
+    Formule VARCHAR2(100) REFERENCES Formules(Formule),
+    DateDepart DATE NOT NULL,
+    DateRetour DATE NOT NULL,
+    KmLoc NUMBER NOT NULL,
+    Montant NUMBER NOT NULL,
+
+    CONSTRAINT check_DateRetour CHECK (DateRetour > DateDepart)
+);
