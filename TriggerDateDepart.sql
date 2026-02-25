4115210bbc4f6738ddcb7f937cfd4b0e38cfae21
Thelan Nguyen <thelannguyen5@gmail.com>
2026-02-25 16:12:38 +0100

    trigger date depart corrigé et check sysdate supprmé dans creaction tables

--- TriggerDateDepart.sql
+++ TriggerDateDepart.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE TRIGGER trigger_date_depart
 BEFORE INSERT OR UPDATE ON Location 
 FOR EACH ROW
 BEGIN
-        IF :NEW.DateDepart < SYSDATE THEN
+        IF :NEW.DateDepart < TRUNC(SYSDATE) THEN
             RAISE_APPLICATION_ERROR(-20001,'La dateDepart ne peut etre dans le passé.');
         END IF;
 
