5bfceb7f77aa76ca3970ec4635a2afed25a09ed6
Thelan Nguyen <thelannguyen5@gmail.com>
2026-03-02 14:19:38 +0100

    trigger insert location ?

--- TriggerInsertLocation.sql
+++ TriggerInsertLocation.sql
@@ -2,21 +2,47 @@ CREATE OR REPLACE TRIGGER triggerInsertSituation
 BEFORE INSERT ON Location
 REFERENCING NEW AS n
 FOR EACH ROW
+DECLARE
+    situationVehicule VARCHAR2(100);
+    nbJoursFormule NUMBER;
+    tarifTarif NUMBER;
+
 BEGIN
-    --verif vehicule.situation == 'disponible'
-    --sinon afficher "Le véhicule vehicule.numveh n'est pas dispo"
 
-    --sequence de fou furieux a faire
+    SELECT Situation
+    INTO situationVehicule
+    FROM Vehicule
+    WHERE NumVeh = :n.NumVeh;
 
-    --location.montant = 
-    --  SELECT t.tarif
-    --  FROM Tarifs t
-    --  JOIN Modeles m ON m.numcat = t.numcat
-    --  JOIN Vehicule v ON v.modele = m.modele
-    --  WHERE t.formule = n.formule
-    --  AND v.numveh = n.numveh
+    if situationVehicule != 'disponible' THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Le véhicule ' || :n.NumVeh || 'n''est pas disponible');
+    END IF;
+
+    if :n.DateDepart < TRUNC(SYSDATE) THEN
+        RAISE_APPLICATION_ERROR(-20002, 'La date de départ ne peut pas être antérieur à la date d''aujourd''hui');
+    END IF;
+
+    :n.NumLoc := 'L' || seqLoc.NEXTVAL;
+    :n.KmLoc := 0;
+
+    SELECT f.NbJours, t.Tarif
+    INTO nbJoursFormule, tarifTarif
+    FROM Formules f
+    JOIN Tarifs t ON t.Formule = f.Formule
+    JOIN Vehicule v ON v.NumVeh = :n.NumVeh
+    JOIN Modeles m ON m.Modele = v.Modele
+    WHERE f.Formule = :n.Formule
+    AND t.NumCat = m.NumCat
+    AND v.NumVeh = :n.NumVeh;
+
+    :NEW.DateRetour := :NEW.DateDepart + v_nb_jours;
+    :NEW.Montant := v_tarif;
+
+    UPDATE Vehicule
+    SET Situation = 'location'
+    WHERE NumVeh = :n.NumVeh;
 
 EXCEPTION
-
-
-END;
+    WHEN NO_DATA_FOUND THEN
+            RAISE_APPLICATION_ERROR(-20003, 'Erreur : Impossible de trouver le tarif ou la formule correspondant au véhicule.');
+    END;
